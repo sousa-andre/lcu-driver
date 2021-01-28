26da336237a1a0bd6ef4c48ea6893c96bf9115f5
XHXIAIEIN <45864744+XHXIAIEIN@users.noreply.github.com>
2021-01-28 12:54:40 +0800

    Update and rename createCustomLobby.py to create_custom_lobby.py

--- examples/create_custom_lobby.py
+++ examples/create_custom_lobby.py
@@ -1,61 +1,57 @@
 from lcu_driver import Connector
-import os, base64
+import os
 
 #---------------------------------------------
 # Get Summoner Data
 #---------------------------------------------
-async def get_summonerdata(connection):
+async def get_summoner_data(connection):
   summoner = await connection.request('GET', '/lol-summoner/v1/current-summoner')
   data = await summoner.json()
-  print(f'{data["displayName"]} Lv.{data["summonerLevel"]}')
+  print(f"{data['displayName']} Lv.{data['summonerLevel']}")
 
 #---------------------------------------------
 # Create Lobby
 #---------------------------------------------
 async def create_lobby(connection):
   custom = {
-    "customGameLobby": {
-      "configuration": {
-        "gameMode": "PRACTICETOOL",
-        "gameMutator": "",
-        "gameServerRegion": "",
-        "mapId": 11, 
-        "mutators": {"id": 1}, 
-        "spectatorPolicy": "AllAllowed", 
-        "teamSize": 5
+    'customGameLobby': {
+      'configuration': {
+        'gameMode': 'PRACTICETOOL',
+        'gameMutator': '',
+        'gameServerRegion': '',
+        'mapId': 11, 
+        'mutators': {'id': 1}, 
+        'spectatorPolicy': 'AllAllowed', 
+        'teamSize': 5
       },
-      "lobbyName": "PRACTICETOOL",
-      "lobbyPassword": ""
+      'lobbyName': 'PRACTICETOOL',
+      'lobbyPassword': ''
     },
-    "isCustom": True
+    'isCustom': True
   }
-  lobby = await connection.request('POST', '/lol-lobby/v2/lobby', data=custom)
-  
+  lobby = await connection.request('POST', '/lol-lobby/v2/lobby', data=custom) 
 
 #---------------------------------------------
-# Add team1 Bots by champion ID
+# Add Team1 Bots By Champion ID
 #---------------------------------------------
-async def add_bot_t1(connection):
- soraka = { "championId": 16, "botDifficulty": "MEDIUM", "teamId": "100"}
+async def add_bots_t1(connection):
+ soraka = { 'championId': 16, 'botDifficulty': 'EASY', 'teamId': '100' }
  await connection.request('POST', '/lol-lobby/v1/lobby/custom/bots', data=soraka)
 
 
 #---------------------------------------------
-# Add team2 Bots by champion Name
+# Add Team2 Bots By Champion Name
 #---------------------------------------------
-async def add_bot_t2(connection):
-  activedata = await connection.request('GET', '/lol-lobby/v2/lobby/custom/available-bots')
-  champions = { bot['name']: bot['id'] for bot in await activedata.json() }
+async def add_bots_t2(connection):
+  available_bots = await connection.request('GET', '/lol-lobby/v2/lobby/custom/available-bots')
+  champions = { bot['name']: bot['id'] for bot in await available_bots.json() }
   
-  team2 = ["Caitlyn", "Blitzcrank", "Darius", "Morgana", "Lux"]
+  team2 = ['Caitlyn', 'Blitzcrank', 'Darius', 'Morgana', 'Lux']
+  team2 = ['黑暗之女', '无极剑圣', '牛头酋长', '武器大师', '堕落天使']
   
   for name in team2:
-    bots = {
-      "championId": champions[name],
-      "botDifficulty": "MEDIUM",
-      "teamId": "200"
-    }
-    await connection.request('POST', '/lol-lobby/v1/lobby/custom/bots', data=bots)
+    bot = { 'championId': champions[name],'botDifficulty': 'MEDIUM', 'teamId': '200' }
+    await connection.request('POST', '/lol-lobby/v1/lobby/custom/bots', data=bot)
 
 
 #---------------------------------------------
@@ -74,8 +70,8 @@ async def get_lockfile(connection):
         file = open(path, 'r')
         text = file.readline().split(':')
         file.close()
+        print(connection.address)
         print(f'riot    {text[3]}')
-        print(f'Etag    {base64.b64encode(self.lock_token.encode())}')
         return text[3]
     return None
 
@@ -87,13 +83,12 @@ connector = Connector()
 
 @connector.ready
 async def connect(connection): 
-  print(connection.address)
+  await get_summoner_data(connection)
   await get_lockfile(connection)
-  await get_summonerdata(connection)
-  await creat_lobby(connection)
-  await add_bot_t1(connection)
-  await add_bot_t2(connection)
-  # await start_game(connection)
+  await create_lobby(connection)
+  await add_bots_t1(connection)
+  await add_bots_t2(connection)
+  await start_game(connection)
 
 
 @connector.close
@@ -104,11 +99,10 @@ async def disconnect(connection):
 
 @connector.ws.register('/lol-lobby/v2/lobby', event_types=('CREATE',))
 async def icon_changed(connection, event):
-    print(f'The summoner {event.data["localMember"]["summonerName"]} created a lobby.')
+    print(f"The summoner {event.data['localMember']['summonerName']} created a lobby.")
 
 
 #---------------------------------------------
 # main
 #---------------------------------------------
 connector.start()
-
