efb52920474f5190f5f8dd7d2070807f23595ad1
André Sousa <andrematosdesousa@gmail.com>
2021-02-02 00:07:18 +0000

    Fix previous commit

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup
 
-<<<<<<< HEAD
-__version__ = '2.1.3'
-=======
 __version__ = '3.0.0a1'
->>>>>>> multiclients
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
