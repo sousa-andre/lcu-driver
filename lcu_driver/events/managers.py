61474488260e1fa9415a20b343b3e022faf3cb70
TheodorStraube <theodor.straube@mailbox.tu-dresden.de>
2021-03-15 18:10:27 +0100

    Update managers.py

--- lcu_driver/events/managers.py
+++ lcu_driver/events/managers.py
@@ -16,6 +16,9 @@ class ConnectorEventManager(ABC):
         return self._handlers
 
     def _set_event(self, event_name, func_or_coro: Union[Callable, Awaitable]):
+        if not asyncio.iscoroutinefunction(func_or_coro):
+                raise TypeError(f'Annotated functions should be coroutines. Use \'async def\'.')
+        
         if event_name in self._handlers:
             self.handlers[event_name].append(func_or_coro)
         else:
@@ -66,7 +69,7 @@ class WebsocketEventManager(ABC):
 
         def register_wrapper(coro_func):
             if not asyncio.iscoroutinefunction(coro_func):
-                raise TypeError(f'Wrapped functions should be coroutines. Use \'async def\'.')
+                raise TypeError(f'Annotated functions should be coroutines. Use \'async def\'.')
             
             for event in event_types:
                 if event not in allowed_events:
